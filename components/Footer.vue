1f89b42446592db805f96009151b2a4145d053eb
Farrel Zandra <farrel.zandra.tif423@polban.ac.id>
2024-05-08 17:08:35 +0700

    adding some folders and files, navbar, header, and footer

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="w-full border-t border-slate-200 py-6">
+        <div class="container">
+            <div class="flex justify-center items-center">
+                <p>&copy; 2024. myApp. All Rights Reserved</p>
+            </div>
+        </div>
+    </footer>
+</template>
