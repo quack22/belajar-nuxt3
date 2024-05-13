e1220eeadaabece61e013ed79230f539b5e27663
Farrel Zandra <farrel.zandra.tif423@polban.ac.id>
2024-05-13 16:51:32 +0700

    developing cart page with some items, including order summary and checkout button

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>Ini Halaman Cart</h1>
+    <section>
+        <div class="container">
+            <div class="py-10 flex gap-6">
+                <div class="w-[70%]">
+                    <div class="flex justify-between items-center pb-7 border-b border-gray-300 mb-6">
+                        <h1 class="text-3x1 font-medium">Shopping Cart</h1>
+                        <p class="text-3x1 font-medium">0 Items</p>
+                    </div>
+                        <div class="flex flex-col gap-6">
+                            <CardsCardCart />
+                        </div>
+                </div>
+                <div class="w-[30%] bg-white shadow-xl h-max p-6">
+                    <h3 class="text-xl font-medium mb-6">Order Summary</h3>
+                    <div class="flex flex-col gap-3 border-b border-gray-300 pb-4">
+                        <div>
+                            <div class="flex gap-4 items-center">
+                                <span class="text-limit limit-1 text-sm">Men's Streetwear Fashion</span>
+                                <span class="text-sm font-semibold">$30</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt-4 flex items-center justify-between mb-6">
+                        <span class="text-base">Total</span>
+                        <span class="text-base font-bold">$30</span>
+                    </div>
+                    <button class="bg-blue-600 text-white text-base font-bold w-full py-2 rounded-lg">Checkout</button>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
